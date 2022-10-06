7ebd7b6df333cfb1e6373a7030bb5397d2406788
Anubhavgupta14 <anubhavgu2002@gmail.com>
2022-10-06 16:13:19 +0530

    Bottom to Top button has been added

--- script.js
+++ script.js
@@ -26,3 +26,22 @@ close.forEach(ele => {
   ele.addEventListener("click", closeMenu);
 }
 )
+
+// bottom to top button
+const backTopElement = document.getElementById("back-top-div");
+      const minScolledAmountToBackTop = 150;
+
+      window.addEventListener("scroll", () => {
+        if (window.scrollY < minScolledAmountToBackTop) {
+          backTopElement.classList.add("hidden");
+        } else {
+          backTopElement.classList.remove("hidden");
+        }
+      });
+
+      const backTop = () => {
+        window.scrollTo(0, 0);
+      };
+
+      backTopElement.addEventListener("click", backTop);
+      ham.addEventListener("click", toggleMenu);
